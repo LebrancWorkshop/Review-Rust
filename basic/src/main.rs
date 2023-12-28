50564f062c90252dbb6bda41f934ee34c122db9f
lebrancconvas <lebrancconvas@gmail.com>
2023-12-28 23:23:23 +0700

    🎉 feat(init): init project

---
+++ basic/src/main.rs
@@ -0,0 +1,7 @@
+fn add(num1: i32, num2: i32) -> i32 {
+    num1 + num2
+}
+
+fn main() {
+    println!("add 1, 2 -> {}", add(1, 2));
+}
---
+++ condition/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a = 99;
+
+    if a < 100 {
+        println!("a < 100");
+    } else if a == 100 {
+        println!("a == 100");
+    } else {
+        println!("a > 100");
+    }
+}
